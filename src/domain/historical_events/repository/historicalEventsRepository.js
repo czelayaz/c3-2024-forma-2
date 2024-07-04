eddc147a1b5cf52fd2fd7154b3ea8441a4888833
Catalina Zelaya <catalina.zelaya@sansano.usm.cl>
2024-07-03 20:57:16 -0400

    control3

--- src/domain/historical_events/repository/historicalEventsRepository.js
+++ src/domain/historical_events/repository/historicalEventsRepository.js
@@ -2,10 +2,15 @@ import eventosJSON from '../../../../dataset/historical_data.json'
 
 exports.getHistoricalEvents = (ocurrence) => {
     let filtered_events = []
+
     if (ocurrence == 'ac'){
         filtered_events = eventosJSON.result.events.filter((evn) => evn.date < 0)
     } else if(ocurrence =='dc'){
         filtered_events = eventosJSON.result.events.filter((evn) => evn.date > 0)
+    } else if(typeof ocurrence === "number"){
+        return ("Solo se aceptan caracteres no numéricos")
+    } else if(ocurrence.lenght !== 2){
+        return ("El input debe ser ac o dc")
     }
     return filtered_events
 }
--- src/domain/historical_events/use_cases/getHistoricalEvents.js
+++ src/domain/historical_events/use_cases/getHistoricalEvents.js
@@ -1,6 +1,12 @@
 import historicalEventsRepository from '../repository/historicalEventsRepository'
 
 exports.getHistoricalEventsByOcurrence = (ctx) => {
+    const ocurrence = ctx.params.ocurrence
+    if (typeof ocurrence === "number"){
+        ctx.body = {message: "Solo se aceptan caracteres no numéricos"}
+    } else if(ocurrence.lenght !== 2){
+        ctx.body = {message: "El input debe ser ac o dc"}
+    }
     ctx.body = historicalEventsRepository.getHistoricalEvents(ctx.params.ocurrence)
     return ctx
 }
